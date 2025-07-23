5f5f85a0e0cc3eca772ff9de75cfa217a10930ae
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-07-23 21:27:15 +0100

    correction

--- libs/core/src/main/java/io/github/orionlibs/core/api/GlobalExceptionHandler.java
+++ libs/core/src/main/java/io/github/orionlibs/core/api/GlobalExceptionHandler.java
@@ -8,10 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
+public class GlobalExceptionHandler
 {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<APIError> onValidationError(MethodArgumentNotValidException ex)
