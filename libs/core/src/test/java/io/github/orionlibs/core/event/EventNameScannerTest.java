1698787b24d426f295a92cac99cdfbef9d956525
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-07-23 14:34:04 +0100

    new tests

---
+++ libs/core/src/test/java/io/github/orionlibs/core/event/EventNameScannerTest.java
@@ -0,0 +1,37 @@
+package io.github.orionlibs.core.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class EventNameScannerTest
+{
+    @Test
+    void scanEventNames()
+    {
+        List<String> allEventNames = EventNameScanner.scanEventNames("io.github.orionlibs.core.event");
+        assertThat(List.of("event-1", "event-2", "event-3")).isEqualTo(allEventNames);
+    }
+
+
+    @Event
+    public static class TestEvent1
+    {
+        public static final String EVENT_NAME = "event-1";
+    }
+
+
+    @Event
+    public static class TestEvent2
+    {
+        public static final String EVENT_NAME = "event-2";
+    }
+
+
+    @Event
+    public static class TestEvent3
+    {
+        public static final String EVENT_NAME = "event-3";
+    }
+}
