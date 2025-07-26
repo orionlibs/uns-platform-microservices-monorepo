832e89dd62cbf585c0cd63d1c87ea046f258691f
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-07-26 18:44:43 +0100

    temp deletion

--- spring-boot-skeleton/src/functionalTest/java/com/onelivery/service1/Test1.java
+++
@@ -1,8 +0,0 @@
-package com.onelivery.service1;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class Test1 {
-    
-}
--- spring-boot-skeleton/src/test/java/com/onelivery/service1/RootControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.onelivery.service1;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class RootControllerTest {
-}
