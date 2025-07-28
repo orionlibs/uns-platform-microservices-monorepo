ec8d2bd52dccc4edbb1bd1621b6202b2849b3362
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-07-28 19:34:58 +0100

    new test

--- services/documents/src/test/java/io/github/orionlibs/document/observability/HealthCheckForDocumentTest.java
+++ services/documents/src/test/java/io/github/orionlibs/document/observability/HealthCheckForDocumentTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.actuate.health.Health;
 
-public class HealthCheckTest
+public class HealthCheckForDocumentTest
 {
     @Test
     void health()
