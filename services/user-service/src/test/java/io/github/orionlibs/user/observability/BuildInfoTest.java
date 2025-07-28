a36551dade3e497b90c93432578d6f2032420d42
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-07-28 14:41:58 +0100

    fixed tests

--- services/user-service/src/test/java/io/github/orionlibs/user/observability/BuildInfoTest.java
+++ services/user-service/src/test/java/io/github/orionlibs/user/observability/BuildInfoTest.java
@@ -4,25 +4,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
 @ActiveProfiles("test")
 public class BuildInfoTest
 {
+    @Autowired
+    BuildInfo contributor;
+
+
     @Test
+    @SuppressWarnings("unchecked")
     void contributesBuildDetails()
     {
-        BuildInfo contributor = new BuildInfo();
         Info.Builder builder = new Info.Builder();
         contributor.contribute(builder);
         Info info = builder.build();
         assertThat(info.getDetails().containsKey("build")).isTrue();
         Object build = info.getDetails().get("build");
         assertThat(build).isInstanceOf(Map.class);
-        @SuppressWarnings("unchecked")
         java.util.Map<String, Object> details = (java.util.Map<String, Object>)build;
-        assertThat(details.get("version")).isEqualTo("1.0.2");
+        assertThat(details.get("version")).isEqualTo("0.0.1");
         assertThat(details.get("environment")).isEqualTo("production");
     }
 }
