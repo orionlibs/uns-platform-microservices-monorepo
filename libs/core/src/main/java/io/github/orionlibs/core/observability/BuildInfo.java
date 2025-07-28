3526fc9cc7dafa506d41c0ff9ba88ca976201312
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-07-28 19:49:24 +0100

    fixed

--- libs/core/src/main/java/io/github/orionlibs/core/observability/BuildInfo.java
+++ libs/core/src/main/java/io/github/orionlibs/core/observability/BuildInfo.java
@@ -3,9 +3,7 @@ package io.github.orionlibs.core.observability;
 import java.util.Map;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
-import org.springframework.stereotype.Component;
 
-@Component
 //GET /actuator/info
 public class BuildInfo implements InfoContributor
 {
