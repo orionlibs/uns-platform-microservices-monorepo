03a54379aa4fcb37427f1e7277515f3a676db587
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-08-01 16:42:26 +0100

    fixed compilation error

--- services/documents/src/main/java/io/github/orionlibs/document/event/EventDocumentDeletedAll.java
+++ services/documents/src/main/java/io/github/orionlibs/document/event/EventDocumentDeletedAll.java
@@ -2,19 +2,18 @@ package io.github.orionlibs.document.event;
 
 import io.github.orionlibs.core.event.Event;
 import java.io.Serializable;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 @Builder
 @Getter
 @Setter
 @Event
 public class EventDocumentDeletedAll implements Serializable
 {
-    public static final String EVENT_NAME = "document-deleted";
+    public static final String EVENT_NAME = "document-deleted-all";
 }
