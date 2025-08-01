80489619060da6bd078342f29d9ac1a2d6d08046
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-08-01 16:43:56 +0100

    fixed

--- services/documents/src/test/java/io/github/orionlibs/document/observability/HealthCheckForDocumentTest.java
+++ services/documents/src/test/java/io/github/orionlibs/document/observability/HealthCheckForDocumentTest.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 public class HealthCheckForDocumentTest
 {
     @Test
