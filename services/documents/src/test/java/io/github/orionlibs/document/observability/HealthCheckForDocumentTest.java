32428ad5bd791bf38952c4c82fdede58b0f97333
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-08-01 16:46:10 +0100

    fixed again

--- services/documents/src/test/java/io/github/orionlibs/document/observability/HealthCheckForDocumentTest.java
+++ services/documents/src/test/java/io/github/orionlibs/document/observability/HealthCheckForDocumentTest.java
@@ -3,6 +3,7 @@ package io.github.orionlibs.document.observability;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,11 +12,14 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 public class HealthCheckForDocumentTest
 {
+    @Autowired
+    private HealthCheckerForDocument healthChecker;
+
+
     @Test
     void health()
     {
-        HealthCheckerForDocument indicator = new HealthCheckerForDocument();
-        Health health = indicator.health();
+        Health health = healthChecker.health();
         assertThat(Health.up().build().getStatus()).isEqualTo(health.getStatus());
         assertThat(health.getDetails().containsKey("custom-check")).isTrue();
         assertThat(health.getDetails().get("custom-check")).isEqualTo("Everything OK");
--- services/user-service/src/test/java/io/github/orionlibs/user/observability/HealthCheckForUserTest.java
+++ services/user-service/src/test/java/io/github/orionlibs/user/observability/HealthCheckForUserTest.java
@@ -3,6 +3,7 @@ package io.github.orionlibs.user.observability;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,11 +12,14 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 public class HealthCheckForUserTest
 {
+    @Autowired
+    private HealthCheckerForUser healthChecker;
+
+
     @Test
     void health()
     {
-        HealthCheckerForUser indicator = new HealthCheckerForUser();
-        Health health = indicator.health();
+        Health health = healthChecker.health();
         assertThat(Health.up().build().getStatus()).isEqualTo(health.getStatus());
         assertThat(health.getDetails().containsKey("custom-check")).isTrue();
         assertThat(health.getDetails().get("custom-check")).isEqualTo("Everything OK");
