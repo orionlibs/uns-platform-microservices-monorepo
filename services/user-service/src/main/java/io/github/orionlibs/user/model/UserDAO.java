9cf3712bba09c00cab1506e41da6106fe180b3cf
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-08-01 13:34:34 +0100

    1

--- services/user-service/src/main/java/io/github/orionlibs/user/model/UserDAO.java
+++ services/user-service/src/main/java/io/github/orionlibs/user/model/UserDAO.java
@@ -1,20 +1,10 @@
 package io.github.orionlibs.user.model;
 
-import io.github.orionlibs.core.cryptology.HMACSHAEncryptionKeyProvider;
-import io.github.orionlibs.core.cryptology.SHAEncodingKeyProvider;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDAO extends JpaRepository<UserModel, UUID>
+public interface UserDAO extends JpaRepository<UserModel, UUID>, UserDAOCustom
 {
-    default Optional<UserModel> findByUsername(String username)
-    {
-        return findByUsernameHash(HMACSHAEncryptionKeyProvider.getNewHMACBase64(username, SHAEncodingKeyProvider.loadKey()));
-    }
-
-
-    Optional<UserModel> findByUsernameHash(String usernameHash);
 }
